0f459b2a094f9647a447315e057b2c681071f001
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-05-27 06:33:37 +0500

    wavy bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 import { ThemeMenuButon } from "~/components/common/theme-menu-button";
-import { user } from "~/db/schema";
-import { db } from "~/db/setup";
+import { WavyBackground } from "~/components/ui/wavy-background";
 
 export default async function HomePage() {
-	const users = await db.select().from(user).all();
-
-	console.log(users);
-
 	return (
 		<main>
-			<section>
-				<h2>Hello, world!</h2>
-				<p>Users: {JSON.stringify(users)}</p>
-				<ThemeMenuButon size="icon" />
-			</section>
+			<WavyBackground>
+				<section>
+					<ThemeMenuButon size="icon" />
+				</section>
+			</WavyBackground>
 		</main>
 	);
 }
---
+++ src/components/ui/wavy-background.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import type { PropsWithChildren } from "react";
+
+import { useEffect, useRef, useState } from "react";
+import { useTheme } from "next-themes";
+import { createNoise3D } from "simplex-noise";
+import { cn } from "~/lib/utils";
+
+export const WavyBackground = ({
+	children,
+	className,
+	containerClassName,
+	colors,
+	waveWidth,
+	blur = 10,
+	speed = "fast",
+	waveOpacity = 0.5,
+}: PropsWithChildren<{
+	className?: string;
+	containerClassName?: string;
+	colors?: string[];
+	waveWidth?: number;
+	blur?: number;
+	speed?: "slow" | "fast";
+	waveOpacity?: number;
+}>) => {
+	const { resolvedTheme } = useTheme();
+
+	const noise = createNoise3D();
+	let w: number;
+	let h: number;
+	let nt: number;
+	let i: number;
+	let x: number;
+	let ctx: CanvasRenderingContext2D | null | undefined;
+	let canvas: HTMLCanvasElement | null;
+
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const getSpeed = () => {
+		switch (speed) {
+			case "slow":
+				return 0.001;
+			case "fast":
+				return 0.002;
+			default:
+				return 0.001;
+		}
+	};
+
+	const init = () => {
+		canvas = canvasRef.current;
+		ctx = canvas?.getContext("2d");
+
+		if (!ctx) {
+			return;
+		}
+
+		w = ctx.canvas.width = window.innerWidth;
+		h = ctx.canvas.height = window.innerHeight;
+		ctx.filter = `blur(${blur}px)`;
+		nt = 0;
+		window.onresize = () => {
+			if (!ctx) {
+				return;
+			}
+
+			w = ctx.canvas.width = window.innerWidth;
+			h = ctx.canvas.height = window.innerHeight;
+			ctx.filter = `blur(${blur}px)`;
+		};
+		render();
+	};
+
+	const waveColors = colors ?? [
+		"#38bdf8",
+		"#818cf8",
+		"#c084fc",
+		"#e879f9",
+		"#22d3ee",
+	];
+	const drawWave = (n: number) => {
+		nt += getSpeed();
+		for (i = 0; i < n; i++) {
+			if (!ctx) {
+				return;
+			}
+
+			ctx.beginPath();
+			ctx.lineWidth = waveWidth || 50;
+			ctx.strokeStyle = waveColors[i % waveColors.length];
+			for (x = 0; x < w; x += 5) {
+				const y = noise(x / 800, 0.3 * i, nt) * 100;
+				ctx.lineTo(x, y + h * 0.5); // adjust for height, currently at 50% of the container
+			}
+			ctx.stroke();
+			ctx.closePath();
+		}
+	};
+
+	let animationId: number | null = null;
+	const render = () => {
+		if (!ctx) {
+			return;
+		}
+
+		ctx.fillStyle = resolvedTheme === "light" ? "white" : "black";
+		ctx.globalAlpha = waveOpacity || 0.5;
+		ctx.fillRect(0, 0, w, h);
+		drawWave(5);
+		animationId = requestAnimationFrame(render);
+	};
+
+	useEffect(() => {
+		init();
+		return () => {
+			if (animationId) {
+				cancelAnimationFrame(animationId);
+			}
+		};
+	}, [init, animationId]);
+
+	const [isSafari, setIsSafari] = useState(false);
+
+	useEffect(() => {
+		// I have got to support it on safari.
+		setIsSafari(
+			typeof window !== "undefined" &&
+				navigator.userAgent.includes("Safari") &&
+				!navigator.userAgent.includes("Chrome"),
+		);
+	}, []);
+
+	return (
+		<div
+			className={cn(
+				"h-screen flex flex-col items-center justify-center",
+				containerClassName,
+			)}
+		>
+			<canvas
+				className="absolute inset-0 z-0"
+				ref={canvasRef}
+				id="canvas"
+				style={{
+					...(isSafari ? { filter: `blur(${blur}px)` } : {}),
+				}}
+			/>
+			<div className={cn("relative z-10", className)}>{children}</div>
+		</div>
+	);
+};
