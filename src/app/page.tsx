ff413382ed51ec874c06bf445be8c1689913fc16
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-05-28 19:05:39 +0500

    drizzle example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import { ThemeMenuButon } from "~/components/common/theme-menu-button";
 import { WavyBackground } from "~/components/ui/wavy-background";
+import { user_table } from "~/db/schema";
+import { db } from "~/db/setup";
 
 export default async function HomePage() {
+	const users = await db.select().from(user_table).all();
+
+	console.log(users);
+
 	return (
 		<main>
 			<WavyBackground>
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const user = sqliteTable("user", {
+export const user_table = sqliteTable("user", {
 	id: text("id").notNull().primaryKey(),
 	name: text("name").notNull(),
 	email: text("email").notNull(),
