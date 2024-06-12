5b19aeb32a96cad8785eb32a27375c58cdfceff8
shayan.workspace <shayan.workspace@gmail.com>
2024-06-12 17:58:44 +0000

    created tables

---
+++ src/db/seeds/aircraft.seed.ts
@@ -0,0 +1,45 @@
+import fs from "node:fs";
+import path from "node:path";
+import { faker } from "@faker-js/faker";
+
+const aircraftManufacturers = [
+	"Airbus",
+	"Boeing",
+	"Embraer",
+	"Bombardier",
+	"Cessna",
+	"Gulfstream",
+	"Dassault",
+	"Mitsubishi",
+	"Sukhoi",
+	"Antonov",
+];
+
+const aircraftModels = [
+	"A320",
+	"737",
+	"E190",
+	"CRJ900",
+	"Citation X",
+	"G650",
+	"Falcon 7X",
+	"SpaceJet",
+	"Superjet 100",
+	"An-124",
+];
+const aircrafts = Array.from({ length: 100 }, () => ({
+	id: `AIRCRAFT-${faker.number.int({ min: 1000, max: 9999 })}`,
+	airlineId: `AIRLINE-${faker.airline.airline().iataCode}`, // reference to airline_table
+	make: faker.helpers.arrayElement(aircraftManufacturers),
+	model: faker.helpers.arrayElement(aircraftModels),
+	capacity: faker.number.int({ min: 100, max: 400 }),
+}));
+
+export default function () {
+	fs.writeFileSync(
+		path.join(__dirname, "aircrafts.json"),
+		JSON.stringify(aircrafts, null, 2),
+	);
+
+	console.log("✅ aircraft data generated.");
+}
---
+++ src/db/seeds/flights.seed.ts
@@ -0,0 +1,33 @@
+import fs from "node:fs";
+import path from "node:path";
+import { faker } from "@faker-js/faker";
+
+const flights = Array.from({ length: 100 }, () => ({
+	// id: `FLIGHT-${faker.number.int({ min: 1000, max: 9999 })}`,
+	id: `${faker.airline.airline().iataCode}${faker.airline.flightNumber({
+		addLeadingZeros: true,
+	})}`, // e.g. 'AA0798'
+
+	airlineId: `AIRLINE-${faker.airline.airline().iataCode}`, // reference to airline_table
+	arrivalAirportId: `AIRPORT-${faker.airline.airport().iataCode}`, // reference to airport_table
+	departureAirportId: `AIRPORT-${faker.airline.airport().iataCode}`, // reference to airport_table
+	aircraftId: `AIRCRAFT-${faker.number.int({ min: 1000, max: 9999 })}`, // reference to aircraft_table
+	departure: faker.date.future().toISOString(),
+	arrival: faker.date.future().toISOString(),
+	status: faker.helpers.arrayElement([
+		"scheduled",
+		"delayed",
+		"cancelled",
+		"departed",
+		"arrived",
+	]),
+}));
+
+export default function () {
+	fs.writeFileSync(
+		path.join(__dirname, "flights.json"),
+		JSON.stringify(flights, null, 2),
+	);
+
+	console.log("✅ flight data generated.");
+}
