80fdf89356fb952dde85e3d1146db5fc58b13b71
shayan.workspace <shayan.workspace@gmail.com>
2024-06-15 11:52:16 +0000

    tables

---
+++ src/app/(dashboard)/accidentRecord/page.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { promises as fs } from "node:fs";
+import { default as path } from "node:path";
+import { z } from "zod";
+
+import { DataTable } from "~/components/ui/data-table";
+import { AccidentRecordNavActions } from "~/app/(dashboard)/accidentRecord/_components/accidentRecords-nav-actions";
+
+import { accidentRecordColumns } from "./_components/accidentRecord-columns";
+import { accidentRecordSchema } from "~/schemas/accidentRecord";
+
+export const metadata: Metadata = {
+	title: "Accident Records",
+};
+
+async function getaccidentRecord() {
+	const data = await fs.readFile(
+		path.join(process.cwd(), "src/db/seeds/accidentRecords.json"),
+	);
+
+	const accidentRecord = JSON.parse(data.toString());
+
+	return z.array(accidentRecordSchema).parse(accidentRecord);
+}
+
+export default async function TasksPage() {
+	const data = await getaccidentRecord();
+
+	return (
+		<>
+			<div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+				<div className="flex items-center justify-between space-y-2">
+					<div>
+						<h2 className="text-2xl font-bold tracking-tight">
+							Accident Records
+						</h2>
+						<p className="text-muted-foreground">
+							Here&apos;s a list of your Accident Records for this month!
+						</p>
+					</div>
+					<div className="flex items-center space-x-2">
+						<AccidentRecordNavActions />
+					</div>
+				</div>
+				<DataTable columns={accidentRecordColumns} data={data} />
+			</div>
+		</>
+	);
+}
---
+++ src/app/(dashboard)/airline/_components/airline-columns.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import type { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "~/components/ui/checkbox";
+import { type Airline } from "~/schemas/airline";
+import { DataTableColumnHeader } from "~/components/ui/data-table-column-header";
+import { AirlineRowActions } from "~/app/(dashboard)/airline/_components/airline-row-actions";
+
+export const airlineColumns: ColumnDef<Airline>[] = [
+	{
+		id: "select",
+		header: ({ table }) => (
+			<Checkbox
+				checked={
+					table.getIsAllPageRowsSelected() ||
+					(table.getIsSomePageRowsSelected() && "indeterminate")
+				}
+				onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+				aria-label="Select all"
+				className="translate-y-[2px]"
+			/>
+		),
+		cell: ({ row }) => (
+			<Checkbox
+				checked={row.getIsSelected()}
+				onCheckedChange={(value) => row.toggleSelected(!!value)}
+				aria-label="Select row"
+				className="translate-y-[2px]"
+			/>
+		),
+		enableSorting: false,
+		enableHiding: false,
+	},
+	{
+		accessorKey: "id",
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Id" />
+		),
+		cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
+		enableSorting: false,
+		enableHiding: false,
+	},
+	{
+		accessorKey: "name",
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Name" />
+		),
+		cell: ({ row }) => <div className="w-[80px]">{row.getValue("name")}</div>,
+		enableSorting: false,
+		enableHiding: false,
+	},
+	{
+		accessorKey: "country",
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Country" />
+		),
+		cell: ({ row }) => (
+			<div className="w-[80px]">{row.getValue("country")}</div>
+		),
+	},
+	{
+		accessorKey: "year",
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Year" />
+		),
+		cell: ({ row }) => <div className="w-[80px]">{row.getValue("year")}</div>,
+	},
+	{
+		id: "actions",
+		cell: ({ row }) => <AirlineRowActions row={row} />,
+	},
+];
